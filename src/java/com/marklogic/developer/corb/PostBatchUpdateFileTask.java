f6ac6daf9bab96085c2a80fb6fa537f9c94e6bea
Bhagat Bandlamudi <bhagat.bandlamudi@marklogic.com>
2014-09-25 10:53:43 -0400

    Added an option to compress the batch output file as zip.

--- src/java/com/marklogic/developer/corb/PostBatchUpdateFileTask.java
+++ src/java/com/marklogic/developer/corb/PostBatchUpdateFileTask.java
@@ -2,10 +2,12 @@ package com.marklogic.developer.corb;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
-import com.marklogic.xcc.ResultSequence;
 
 /**
  * @author Bhagat Bandlamudi, MarkLogic Corporation
@@ -34,18 +36,76 @@ public class PostBatchUpdateFileTask extends ExportBatchToFileTask {
 		}
 	}
 	
+	protected void moveFile(String source, String dest) throws IOException{
+		if(!source.equals(dest)){
+			File srcFile = new File(exportDir,source);
+			if(srcFile.exists()){
+				File destFile = new File(exportDir,dest);
+				if(destFile.exists()){
+					destFile.delete();				
+				}
+				srcFile.renameTo(destFile);
+			}
+		}
+	}
+	
 	protected void moveFile() throws IOException{
 		String partFileName = getPartFileName();
 		String finalFileName = getFileName();
-		if(!partFileName.equals(finalFileName)){
-			File partFile = new File(exportDir,partFileName);
-			if(partFile.exists()){
-				File finalFile = new File(exportDir,finalFileName);
-				if(finalFile.exists()){
-					finalFile.delete();				
-				}
-				partFile.renameTo(finalFile);
+		moveFile(partFileName,finalFileName);
+	}
+	
+	protected void compressFile() throws IOException{
+		if("true".equalsIgnoreCase(getProperty("EXPORT_FILE_AS_ZIP"))){
+			String outFileName = getFileName();
+			String outZipFileName = outFileName+".zip";
+			
+			String partZipFileName = outZipFileName;
+			String partExt = getProperty("EXPORT-FILE-PART-EXT");
+			if(partExt != null && partExt.length() > 0){
+				if(!partExt.startsWith(".")) partExt = "."+partExt;
+				partZipFileName = outZipFileName+partExt;
 			}
+			
+			File outFile = new File(exportDir,outFileName);
+			File zipFile = new File(exportDir,partZipFileName);
+			
+			ZipOutputStream zos = null;
+			FileOutputStream fos = null;
+			FileInputStream fis = null;
+			
+			try{				
+				if(outFile.exists()){					
+					if(zipFile.exists()){
+						zipFile.delete();
+					}
+					
+					fos = new FileOutputStream(zipFile);
+					zos = new ZipOutputStream(fos);
+					
+					ZipEntry ze = new ZipEntry(outFileName);
+					zos.putNextEntry(ze);
+					
+					byte[] buffer = new byte[2048];
+					fis = new FileInputStream(outFile);
+					int len;
+					while ((len = fis.read(buffer)) > 0){
+						zos.write(buffer, 0, len);
+					}
+					
+					zos.closeEntry();
+					zos.flush();
+				}
+			}finally{
+				if(zos != null) zos.close();
+				if(fis != null) fis.close();
+				if(fos != null) fos.close();
+			}
+			//move the file if required
+			moveFile(partZipFileName,outZipFileName);
+			
+			//now that we have everything, delete the uncompressed output file
+			if(outFile.exists()) outFile.delete();
 		}
 	}
 	
@@ -53,6 +113,7 @@ public class PostBatchUpdateFileTask extends ExportBatchToFileTask {
     	invokeModule();
 		writeBottomContent();
 		moveFile();
+		compressFile();
     	return TRUE;
     }
 }
