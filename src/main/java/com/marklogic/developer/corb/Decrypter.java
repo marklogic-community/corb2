e20eb3afdb2c6a467f9a22beba1e03e2096e0810
Mads Hansen <mads.hansen@marklogic.com>
2015-12-27 08:38:20 -0500

    removed unnecessary public modifiers

--- src/main/java/com/marklogic/developer/corb/Decrypter.java
+++ src/main/java/com/marklogic/developer/corb/Decrypter.java
@@ -22,7 +22,7 @@ import java.io.IOException;
 import java.util.Properties;
 
 public interface Decrypter {
-	public void init(Properties properties) throws IOException, ClassNotFoundException;
-	public String getConnectionURI(String uri, String username, String password, String host, String port, String dbname);
-	public String decrypt(String property, String value);
+	void init(Properties properties) throws IOException, ClassNotFoundException;
+	String getConnectionURI(String uri, String username, String password, String host, String port, String dbname);
+	String decrypt(String property, String value);
 }
