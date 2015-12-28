ecabf5b72eaa2795d923d22dbdc53d8bdc4774df
Mads Hansen <mads.hansen@marklogic.com>
2015-12-27 21:52:18 -0500

    correct spelling

--- src/main/java/com/marklogic/developer/corb/AbstractTask.java
+++ src/main/java/com/marklogic/developer/corb/AbstractTask.java
@@ -367,7 +367,7 @@ public abstract class AbstractTask implements Task {
             try {
                 intVal = Integer.parseInt(value);
             } catch (Exception exc) {
-                LOG.log(Level.WARNING, "Unable to parese ''{0}'' value ''{1}'' as an int", new Object[]{key, value});
+                LOG.log(Level.WARNING, "Unable to parse '{0}' value '{1}' as an int", new Object[]{key, value});
             }
         }
         return intVal;
