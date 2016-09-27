25f4d918654f9847e02ad59a4f3ffd8eee6451f8
Mads Hansen <mads.hansen@marklogic.com>
2016-09-26 21:46:35 -0400

    remove unused import

--- src/test/java/com/marklogic/developer/corb/ModuleExecutorTest.java
+++ src/test/java/com/marklogic/developer/corb/ModuleExecutorTest.java
@@ -22,7 +22,6 @@ import com.marklogic.developer.TestHandler;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
