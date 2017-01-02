46d5eec5c5329fe679d09f982d130bf43b7bb0f4
Mads Hansen <mads.hansen@marklogic.com>
2017-01-02 15:05:06 -0500

    remove catch for IOException in FileUrisXMLLoader

--- src/main/java/com/marklogic/developer/corb/FileUrisXMLLoader.java
+++ src/main/java/com/marklogic/developer/corb/FileUrisXMLLoader.java
@@ -200,7 +200,7 @@ public class FileUrisXMLLoader extends AbstractUrisLoader {
         if (nextUri == null) {
             try {
                 nextUri = readNextNode();
-            } catch (IOException | CorbException exc) {
+            } catch (CorbException exc) {
                 throw new CorbException(EXCEPTION_MSG_PROBLEM_READING_XML_FILE, exc);
             }
         }
@@ -216,7 +216,7 @@ public class FileUrisXMLLoader extends AbstractUrisLoader {
         } else {
             try {
                 node = readNextNode();
-            } catch (IOException | CorbException exc) {
+            } catch (CorbException exc) {
                 throw new CorbException(EXCEPTION_MSG_PROBLEM_READING_XML_FILE, exc);
             }
         }
