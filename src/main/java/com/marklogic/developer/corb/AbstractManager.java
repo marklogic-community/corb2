f05365c598aa275549d7a1e5b517e5de167fde04
Mads Hansen <mads.hansen@marklogic.com>
2017-08-08 00:25:23 -0400

    remove unused imports

--- src/main/java/com/marklogic/developer/corb/AbstractManager.java
+++ src/main/java/com/marklogic/developer/corb/AbstractManager.java
@@ -69,9 +69,7 @@ import static com.marklogic.developer.corb.util.StringUtils.buildModulePath;
 import static com.marklogic.developer.corb.util.StringUtils.isBlank;
 import static com.marklogic.developer.corb.util.StringUtils.isInlineModule;
 import static com.marklogic.developer.corb.util.StringUtils.isInlineOrAdhoc;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.logging.Level;
 
 public abstract class AbstractManager {
