9ea9c976ef5fbf10110fefec9bdaeae315f32908
Mads Hansen <mads.hansen@marklogic.com>
2017-10-14 16:01:51 -0400

    InvocationTargetException wrapping real exception
    if we get an InvocationTargetException, unwrap it and either handle the
    ServerConnectionException or throw the real exception

--- src/main/java/com/marklogic/developer/corb/DefaultContentSourcePool.java
+++ src/main/java/com/marklogic/developer/corb/DefaultContentSourcePool.java
@@ -5,6 +5,7 @@ import static java.util.logging.Level.INFO;
 import static java.util.logging.Level.WARNING;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.ArrayList;
@@ -149,7 +150,7 @@ public class DefaultContentSourcePool extends AbstractContentSourcePool{
         if (contentSourceList.contains(contentSource)) {
             String hostname = contentSource.getConnectionProvider().getHostName();
             int port = contentSource.getConnectionProvider().getPort();
-            
+
             LOG.log(WARNING, "Removing the MarkLogic server at {0}:{1} from the content source pool.", new Object[]{hostname,port});
 	        contentSourceList.remove(contentSource);
 	        connectionCountsMap.remove(contentSource);
@@ -173,11 +174,11 @@ public class DefaultContentSourcePool extends AbstractContentSourcePool{
 
     @Override
     public ContentSource[] getAllContentSources() {
-    		return contentSourceList.toArray(new ContentSource[contentSourceList.size()]);
+        return contentSourceList.toArray(new ContentSource[contentSourceList.size()]);
     }
 
     protected boolean isLoadPolicy() {
-    		return CONNECTION_POLICY_LOAD.equals(connectionPolicy);
+        return CONNECTION_POLICY_LOAD.equals(connectionPolicy);
     }
 
     synchronized protected void hold(ContentSource cs) {
@@ -187,7 +188,7 @@ public class DefaultContentSourcePool extends AbstractContentSourcePool{
     }
 
     synchronized protected void release(ContentSource cs) {
-    		Integer count = connectionCountsMap.get(cs);
+        Integer count = connectionCountsMap.get(cs);
         count = (count == null || count > 0 ) ? 0 : count - 1;
         connectionCountsMap.put(cs, count);
     }
@@ -198,7 +199,7 @@ public class DefaultContentSourcePool extends AbstractContentSourcePool{
 	}
 
     synchronized protected void error(ContentSource cs) {
-    		Integer count = errorCountsMap.get(cs);
+        Integer count = errorCountsMap.get(cs);
         count = count == null ? 1 : count + 1;
         errorCountsMap.put(cs, count);
         errorTimeMap.put(cs, System.currentTimeMillis());
@@ -208,7 +209,7 @@ public class DefaultContentSourcePool extends AbstractContentSourcePool{
         int port = cs.getConnectionProvider().getPort();
         LOG.log(WARNING, "Connection error count for host {0}:{1} is {2}. Max limit is {3}.", new Object[]{hostname,port,count,limit});
         if (count > limit){
-        		remove(cs);
+            remove(cs);
         }
 	}
 
@@ -237,7 +238,7 @@ public class DefaultContentSourcePool extends AbstractContentSourcePool{
 		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 			Object obj = method.invoke(target, args);
 
-			if(obj != null && method.getName().equals("newSession") && obj instanceof Session) {
+			if (obj != null && method.getName().equals("newSession") && obj instanceof Session) {
 				obj = createSessionProxy((Session)obj);
 			}
 
@@ -288,26 +289,26 @@ public class DefaultContentSourcePool extends AbstractContentSourcePool{
                     }
 				}
 				return obj;
-			} catch(Exception exc) {
+			} catch (InvocationTargetException exc) {
 				if (exc.getCause() instanceof ServerConnectionException) {
-					csp.error(cs);
+                    csp.error(cs);
 
-					if (csp.isLoadPolicy() && (isSubmitRequest(method) || isInsertContent(method))) {
-						csp.release(cs); //we should don't this in finally clause.
-					}
+                    if (csp.isLoadPolicy() && (isSubmitRequest(method) || isInsertContent(method))) {
+                        csp.release(cs); //we should don't this in finally clause.
+                    }
 
-					int retryLimit = csp.getConnectRetryLimit();
-					if (isSubmitRequest(method) && attempts < retryLimit) {
-						LOG.log(INFO, "Submit request failed {0} times. Max Limit is {1}. Retrying..", new Object[]{attempts, retryLimit});
-						return submitAsNewRequest(args);
-					} else if (isInsertContent(method) && attempts < retryLimit) {
-						LOG.log(INFO, "Insert content failed {0} times. Max Limit is {1}. Retrying..", new Object[]{attempts, retryLimit});
-						return insertAsNewRequest(args);
-					} else {
-						throw exc;
-					}
-				} else {
-					throw exc;
+                    int retryLimit = csp.getConnectRetryLimit();
+                    if (isSubmitRequest(method) && attempts < retryLimit) {
+                        LOG.log(INFO, "Submit request failed {0} times. Max Limit is {1}. Retrying..", new Object[]{attempts, retryLimit});
+                        return submitAsNewRequest(args);
+                    } else if (isInsertContent(method) && attempts < retryLimit) {
+                        LOG.log(INFO, "Insert content failed {0} times. Max Limit is {1}. Retrying..", new Object[]{attempts, retryLimit});
+                        return insertAsNewRequest(args);
+                    } else {
+                        throw exc;
+                    }
+                } else {
+				    throw exc.getCause();
 				}
 			}
 		}
