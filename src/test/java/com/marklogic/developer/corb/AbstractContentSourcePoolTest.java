05ad3c7bb29c7f247004352dbaaffaaa9bfeaf8f
Bhagat Bandlamudi <bhagat.bandlamudi@marklogic.com>
2017-10-13 21:39:21 -0400

    Added test cases for AbstractContentSourcePool

--- src/test/java/com/marklogic/developer/corb/AbstractContentSourcePoolTest.java
+++ src/test/java/com/marklogic/developer/corb/AbstractContentSourcePoolTest.java
@@ -11,6 +11,8 @@ import static org.mockito.Mockito.mock;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Properties;
 
 public class AbstractContentSourcePoolTest {
@@ -31,18 +33,55 @@ public class AbstractContentSourcePoolTest {
 	}
 	
 	@Test
-	public void testNoInit() {
+	public void testInitWithTrustAnyOneSSLConfig() throws KeyManagementException, NoSuchAlgorithmException {
+		SSLConfig sslConfig = new TrustAnyoneSSLConfig();
+		Properties props = new Properties();
+		props.put("foo", "bar");
+		ContentSourcePoolImpl csp = new ContentSourcePoolImpl();
+		csp.init(props, sslConfig);
+		assertEquals(sslConfig,csp.getSSLConfig());
+		assertEquals("bar",csp.getProperty("foo"));
+		assertArrayEquals(new TrustAnyoneSSLConfig().getSecurityOptions().getEnabledProtocols(), csp.getSecurityOptions().getEnabledProtocols());
+	}
+	
+	@Test
+	public void testNoInit() throws KeyManagementException, NoSuchAlgorithmException {
 		ContentSourcePoolImpl csp = new ContentSourcePoolImpl();
 		assertTrue(csp.getSSLConfig().getClass().equals(TrustAnyoneSSLConfig.class));
 		assertNotNull(csp.properties);
+		assertNotNull(csp.getSecurityOptions());
+		assertArrayEquals(new TrustAnyoneSSLConfig().getSecurityOptions().getEnabledProtocols(), csp.getSecurityOptions().getEnabledProtocols());
 	}
 	
+	@Test
 	public void testGetIntPropertyFromSystemProperty() {
 		System.setProperty("foo", "123");
 		ContentSourcePoolImpl csp = new ContentSourcePoolImpl();
 		assertEquals(123,csp.getIntProperty("foo"));
 	}
 	
+	@Test
+    public void testPrepareContentSource() {
+		ContentSourcePoolImpl csp = new ContentSourcePoolImpl();
+        ContentSource cs = csp.createContentSource("xcc://user:pass@localhost:8000");
+        assertEquals("localhost",cs.getConnectionProvider().getHostName());
+        assertEquals(8000,cs.getConnectionProvider().getPort());
+    }
+
+    @Test
+    public void testPrepareContentSourceSecureXCC() {
+    		ContentSourcePoolImpl csp = new ContentSourcePoolImpl();
+        ContentSource cs = csp.createContentSource("xccs://user:pass@localhost:8000");
+        assertEquals("localhost",cs.getConnectionProvider().getHostName());
+        assertEquals(8000,cs.getConnectionProvider().getPort());
+    }
+    
+    @Test
+    public void testPrepareContentSourceNoScheme() throws CorbException {
+    		ContentSourcePoolImpl csp = new ContentSourcePoolImpl();
+        ContentSource cs = csp.createContentSource("//user:pass@localhost:8000");
+        assertNull(cs);
+    }
 	
 	private class ContentSourcePoolImpl extends AbstractContentSourcePool{
 		@Override public void init(Properties properties, SSLConfig sslConfig, String[] connectionStrings) {}
