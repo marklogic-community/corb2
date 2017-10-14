857ae00d5f88bfb27584100c34790a02a78ac5bc
Mads Hansen <mads.hansen@marklogic.com>
2017-10-13 22:00:23 -0400

    remove unused imports

--- src/test/java/com/marklogic/developer/corb/AbstractContentSourceManagerTest.java
+++ src/test/java/com/marklogic/developer/corb/AbstractContentSourceManagerTest.java
@@ -1,15 +1,26 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2004-2017 MarkLogic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * The use of the Apache License does not indicate that this project is
+ * affiliated with the Apache Software Foundation.
  */
 package com.marklogic.developer.corb;
 
 import com.marklogic.xcc.ContentSource;
 import java.util.Properties;
 import org.junit.Test;
-
-import javax.xml.bind.annotation.XmlType;
 
 import static com.marklogic.developer.corb.AbstractContentSourcePool.DEFAULT_CONNECTION_RETRY_INTERVAL;
 import static com.marklogic.developer.corb.AbstractContentSourcePool.DEFAULT_CONNECTION_RETRY_LIMIT;
