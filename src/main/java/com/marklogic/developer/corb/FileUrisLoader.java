b75eb275f9b1c74f7878d4ed1dcf89e1c324ede0
Mads Hansen <mads.hansen@marklogic.com>
2017-11-01 00:20:12 -0400

    ensure that one of the numbers is evaluated as long

--- src/main/java/com/marklogic/developer/corb/FileUrisLoader.java
+++ src/main/java/com/marklogic/developer/corb/FileUrisLoader.java
@@ -46,7 +46,7 @@ public class FileUrisLoader extends AbstractUrisLoader {
         }
         try (LineNumberReader lnr = new LineNumberReader(new FileReader(fileName))) {
             lnr.skip(Long.MAX_VALUE);
-            setTotalCount(lnr.getLineNumber() + 1);
+            setTotalCount(lnr.getLineNumber() + 1L);
             //these are closed in the close() method
             fileReader = new FileReader(fileName);
             bufferedReader = new BufferedReader(fileReader);
