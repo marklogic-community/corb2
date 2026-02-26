f7ccba61bd5c798ec4e449b2f12f2336f77ef294
Mads Hansen <mads.hansen@marklogic.com>
2026-02-25 19:38:26 -0500

    Cleanup - use static reference to test method

--- src/test/java/com/marklogic/developer/corb/BaseMonitorTest.java
+++ src/test/java/com/marklogic/developer/corb/BaseMonitorTest.java
@@ -26,25 +26,21 @@ public class BaseMonitorTest {
 
     @Test
     public void formatTransactionsPerSecond() {
-        Manager manager = new Manager();
-        BaseMonitor monitor = new BaseMonitor(manager);
-        assertEquals("0.54", monitor.formatTransactionsPerSecond(0.54321));
-        assertEquals("5", monitor.formatTransactionsPerSecond(5.4321));
-        assertEquals("54", monitor.formatTransactionsPerSecond(54.321));
-        assertEquals("543", monitor.formatTransactionsPerSecond(543.21));
-        assertEquals("5,432", monitor.formatTransactionsPerSecond(5432.1));
-        assertEquals("54,321", monitor.formatTransactionsPerSecond(54321));
+        assertEquals("0.54", BaseMonitor.formatTransactionsPerSecond(0.54321));
+        assertEquals("5", BaseMonitor.formatTransactionsPerSecond(5.4321));
+        assertEquals("54", BaseMonitor.formatTransactionsPerSecond(54.321));
+        assertEquals("543", BaseMonitor.formatTransactionsPerSecond(543.21));
+        assertEquals("5,432", BaseMonitor.formatTransactionsPerSecond(5432.1));
+        assertEquals("54,321", BaseMonitor.formatTransactionsPerSecond(54321));
     }
 
     @Test
     public void formatTransactionsPerSecondWithoutGrouping() {
-        Manager manager = new Manager();
-        BaseMonitor monitor = new BaseMonitor(manager);
-        assertEquals("0.54", monitor.formatTransactionsPerSecond(0.54321, false));
-        assertEquals("5", monitor.formatTransactionsPerSecond(5.4321, false));
-        assertEquals("54", monitor.formatTransactionsPerSecond(54.321, false));
-        assertEquals("543", monitor.formatTransactionsPerSecond(543.21, false));
-        assertEquals("5432", monitor.formatTransactionsPerSecond(5432.1, false));
-        assertEquals("54321", monitor.formatTransactionsPerSecond(54321, false));
+        assertEquals("0.54", BaseMonitor.formatTransactionsPerSecond(0.54321, false));
+        assertEquals("5", BaseMonitor.formatTransactionsPerSecond(5.4321, false));
+        assertEquals("54", BaseMonitor.formatTransactionsPerSecond(54.321, false));
+        assertEquals("543", BaseMonitor.formatTransactionsPerSecond(543.21, false));
+        assertEquals("5432", BaseMonitor.formatTransactionsPerSecond(5432.1, false));
+        assertEquals("54321", BaseMonitor.formatTransactionsPerSecond(54321, false));
     }
 }
